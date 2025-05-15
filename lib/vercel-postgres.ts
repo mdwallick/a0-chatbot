eddd9bb24daaa1481c368364bc3839a3b0abf12d
Mike Wallick <mike@wallick.net>
2025-05-15 14:38:01 -0500

    prepping for Vercel deployment

---
+++ lib/vercel-postgres.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+import { sql } from "@vercel/postgres";
+
+// Use Vercel Postgres in production, local PostgreSQL in development
+const prisma = new PrismaClient()
+
+export { sql, prisma }

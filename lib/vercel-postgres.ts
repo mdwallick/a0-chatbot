39e265cb99c2d33265267968a70d2f182f36d3ac
Mike Wallick <mike@wallick.net>
2025-05-16 15:34:55 -0500

    upates vercel postgres config

--- lib/vercel-postgres.ts
+++ lib/vercel-postgres.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { sql } from "@vercel/postgres";
+import { PrismaClient } from "@prisma/client"
+import { sql } from "@vercel/postgres"
 
 // Use Vercel Postgres in production, local PostgreSQL in development
 const prisma = new PrismaClient()
